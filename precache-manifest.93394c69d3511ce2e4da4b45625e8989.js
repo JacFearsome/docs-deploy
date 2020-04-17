4bb9d17d101d263c3e05581b2d953fcc5d2eee8b
Jesse Russell <jrussellfreelance@gmail.com>
2020-04-17 11:41:26 -0400

    Updates

---
+++ precache-manifest.93394c69d3511ce2e4da4b45625e8989.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef646012d30f07c929f9ddd69549ab12",
+    "url": "/doc-deploy/index.html"
+  },
+  {
+    "revision": "8cbb458eb23b09277fef",
+    "url": "/doc-deploy/static/css/2.0dafb0b8.chunk.css"
+  },
+  {
+    "revision": "295e8a7dabe15c7e60b6",
+    "url": "/doc-deploy/static/css/main.ca83646a.chunk.css"
+  },
+  {
+    "revision": "8cbb458eb23b09277fef",
+    "url": "/doc-deploy/static/js/2.29f34d80.chunk.js"
+  },
+  {
+    "revision": "0a8b59af9bc84e5a7421b6d864f5a504",
+    "url": "/doc-deploy/static/js/2.29f34d80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "295e8a7dabe15c7e60b6",
+    "url": "/doc-deploy/static/js/main.aa6ee9ee.chunk.js"
+  },
+  {
+    "revision": "c801224dda031b3217ef",
+    "url": "/doc-deploy/static/js/runtime-main.09e6376d.js"
+  },
+  {
+    "revision": "05f1cdadfe476395f60e233b15c22155",
+    "url": "/doc-deploy/static/media/icons-16.05f1cdad.eot"
+  },
+  {
+    "revision": "3c1c220e7a18286503fb431c7a7fe183",
+    "url": "/doc-deploy/static/media/icons-16.3c1c220e.woff"
+  },
+  {
+    "revision": "3cde8748332d1de6b1ae1c2dc5850754",
+    "url": "/doc-deploy/static/media/icons-16.3cde8748.ttf"
+  },
+  {
+    "revision": "0a5c76518a68c185baa2c6744456918c",
+    "url": "/doc-deploy/static/media/icons-20.0a5c7651.eot"
+  },
+  {
+    "revision": "51ec31f302d0072808e1f83f85fea4cd",
+    "url": "/doc-deploy/static/media/icons-20.51ec31f3.ttf"
+  },
+  {
+    "revision": "cef8cdbb9d0ba82e6e19fb0eeba2ac3d",
+    "url": "/doc-deploy/static/media/icons-20.cef8cdbb.woff"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/doc-deploy/precache-manifest.93394c69d3511ce2e4da4b45625e8989.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/doc-deploy/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
