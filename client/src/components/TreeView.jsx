74cb3f3d0d112dfcd63f51849fa67479bc947c3e
Jesse Russell <jrussellfreelance@gmail.com>
2020-04-18 20:04:45 -0400

    added editor and preview

--- client/src/components/TreeView.jsx
+++ client/src/components/TreeView.jsx
@@ -1,6 +1,5 @@
-import { Link } from "react-router-dom";
-import PropTypes from "prop-types";
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 import {
     Alignment,
     Button,
