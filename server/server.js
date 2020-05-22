c158cd4e5852eaf388c08326fc47f0ec24cd0d20
Maddix <starcraftd@gmail.com>
2020-05-21 22:29:35 -0400

    WIP - Tidied up a bit.

--- server/server.js
+++ server/server.js
@@ -1,86 +1,86 @@
-const express = require("express");
-const axios = require('axios')
-const cors = require('cors')
-const randomString = require('randomstring');
-const Server = require("http").Server;
-const session = require("express-session");
-const redis = require('redis')
-const RedisStore = require("connect-redis")(session);
-const config = require('./config.json');
+const
+	// Configuration
+	config = require('./config.json'),
+	session = require("express-session"),
+	// Database
+	redis = require('redis'),
+	RedisStore = require("connect-redis")(session),
+	redisClient = redis.createClient(6379, config.redisHost),
+	// Libraries
+	express = require("express"),
+	cors = require('cors'),
+	axios = require('axios'),
+	// Server
+	app = express(),
+	server = require('http').Server(app),
+	SocketIO = require("socket.io")(server);
+	
+/*
+ * Create a user session
+*/
 
-const app = express();
-const server = Server(app);
-const sio = require("socket.io")(server);
-const redisClient = redis.createClient(6379, config.redisHost)
-let sessionMiddleware = session({
+const sessionMiddleware = session({
     store: new RedisStore({client: redisClient}),
     secret: config.redisSecret,
 });
 
-sio.use(function(socket, next) {
-    sessionMiddleware(socket.request, socket.request.res || {}, next);
+/*
+ * Setup Socket.io
+*/
+SocketIO.use((socket, next) => { sessionMiddleware(socket.request, socket.request.res || {}, next); });
+SocketIO.sockets.on("connection", socket => {
+    if (socket.request.session.github_user) {
+        socket.emit('user_data', {
+			access_token: socket.request.session.access_token,
+			user: socket.request.session.github_user
+		});
+    }
 });
 
+/*
+ * Setup Express
+*/
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));// Accept requests from our client
-app.use(cors({
-  origin: config.clientOrigin
-}))
+app.use(cors({ origin: config.clientOrigin }));
 app.use(sessionMiddleware);
 
-app.get(`/`, (req, res) => {
-    res.send(req.session);
-});
-
-app.get(`/login`, (req, res) => {
-    res.redirect(`https://github.com/login/oauth/authorize?client_id=${config.clientId}&scope=repo%20user`)
-});
-
+/*
+ * Routes
+*/
+app.get(`/`, (req, res) => { res.send(req.session); });
+app.get(`/login`, (req, res) => { res.redirect(`https://github.com/login/oauth/authorize?client_id=${config.clientId}&scope=repo%20user`); });
 app.get(`/signout`, (req, res) => {
   req.session.github_user = {};
   req.session.access_token = "";
-  res.redirect(config.clientOrigin)
-  res.end()
-});
-
-sio.sockets.on("connection", function(socket) {
-    if (socket.request.session.github_user) {
-        socket.emit('user_data', {access_token: socket.request.session.access_token, user: socket.request.session.github_user})
-    }
+  res.redirect(config.clientOrigin);
+  res.end();
 });
 
 app.get(`/callback`, (req, res) => {
     const requestToken = req.query.code;
     axios({
-      method: 'post',
-      url: `https://github.com/login/oauth/access_token?client_id=${config.clientId}&client_secret=${config.clientSecret}&code=${requestToken}`,
-      // Set the content type header, so that we get the response in JSON
-      headers: {
-           accept: 'application/json'
-      }
-    }).then((response) => {
-      const accessToken = response.data.access_token
-      axios({
-        method: 'get',
-        url: `https://api.github.com/user`,
-        // Set the content type header, so that we get the response in JSON
-        headers: {
-             accept: 'application/json',
-             Authorization: `Bearer ${accessToken}`
-        }
-      }).then(function (user_res) {
-            req.session.access_token = accessToken;
-            req.session.github_user = user_res.data;
-            res.redirect(config.clientOrigin);
-      })
-      .catch(function (error) {
-        res.json({error: error})
-      });
-    })
-    .catch(function (error) {
-      res.json({error: error})
-    })
+    	method: 'post',
+		url: `https://github.com/login/oauth/access_token?client_id=${config.clientId}&client_secret=${config.clientSecret}&code=${requestToken}`,
+		headers: { accept: 'application/json' } // Set the content type header, so that we get the response in JSON
+	}).then(response => {
+		const accessToken = response.data.access_token;
+		axios({
+			method: 'get',
+			url: 'https://api.github.com/user',
+			headers: { // Set the content type header, so that we get the response in JSON
+				 accept: 'application/json',
+				 Authorization: `Bearer ${accessToken}`
+			}
+		}).then(user_res => {
+			req.session.access_token = accessToken;
+			req.session.github_user = user_res.data;
+			res.redirect(config.clientOrigin);
+		}).catch(error => res.json({error}));
+	}) .catch(error => res.json({error}));
 });
 
-
+/*
+ * Start the server
+*/
 server.listen(config.port, () => console.log(`[docs-server] running on port ${config.port}`));
